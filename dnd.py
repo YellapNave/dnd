2bfa5b3d109997a5e3832a457eca210fb2957442
Evan Davis-Palley <edavis-palley@wifi-131-179-4-15.host.ucla.edu>
2018-07-20 12:03:15 -0700

    add script

---
+++ dnd.py
@@ -0,0 +1,88 @@
+import random
+
+# Take an input and remove all the spaces, then convert it entirely to lowercase.
+def cleanse(input):
+    return (input.replace(" ","")).lower()
+
+# Take a basic dice format, such as 2d4, and output an array of integers equal
+# to the rolls that result. If the input contains more than one 'd', or there is
+# a single d with something other than numbers on either side of it, an empty
+# list is returned.
+def parseDice(form):
+    if form.count('+') == 0 and form.count('-') == 0:
+        if form.count('d') >= 2:
+            return []
+        else:
+            request = form.split('d')
+
+        for i in request[0]:
+            if i.isdigit() == False:
+                return []
+        for i in request[1]:
+            if i.isdigit() == False:
+                return []
+
+        n = int(request[0])
+        sides = int(request[1])
+        rolls = []
+        for i in range(n):
+            rolls.append(random.randint(1,sides))
+        return (rolls)
+
+while True:
+
+    module = raw_input("Select module:\n1. Dice roller\n2. Test dice roller\n")
+    module = cleanse(module)
+
+    if module == '1':
+        print("Dice roller selected.")
+        go = True
+
+        while go:
+            input = raw_input("Roll condition: ")
+            input = cleanse(input)
+
+            if input == 'exit' or input == 'quit':
+                go = False
+                print("Quitting module.")
+                break
+            elif input.count('+') == 1:
+                form = input.split('+')
+                dice = form[0]
+                bonus = int(cleanse(form[1]))
+                verify = True
+            elif input.count('-') == 1:
+                form = input.split('-')
+                request = form[0].split('d')
+                bonus = 0 - int(form[1].replace(" ",""))
+                verify = True
+            else:
+                if input.count('-') > 1 or input.count('+') > 1:
+                    print("Error - format not recognized.")
+                    verify = False
+                    break
+                else:
+                    verify = True
+                    dice = input
+                    bonus = 0
+
+            rolls = parseDice(dice)
+            if rolls == []:
+                print("Error - format not recognized.")
+                verify = False
+
+            if verify:
+                if bonus < 0:
+                    operator = " - "
+                else:
+                    operator = " + "
+                result = str(sum(rolls) + bonus) + ': ' + ' '.join((str(i) for i in rolls))
+                if bonus != 0:
+                    print result + operator + str(abs(bonus))
+                else:
+                    print result
+
+    elif module == 'exit' or module == 'quit' :
+        print("Goodbye.")
+        break
+    else: print("Sorry, module not found.")
